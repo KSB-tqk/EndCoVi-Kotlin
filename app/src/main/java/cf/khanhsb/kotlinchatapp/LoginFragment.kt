0bbff64a9b0f0a71d5053aecdf74865176a6614e
KSB-tqk <linh05602@gmail.com>
2021-08-25 18:01:56 +0700

    signup ui

--- app/src/main/java/cf/khanhsb/kotlinchatapp/LoginFragment.kt
+++ app/src/main/java/cf/khanhsb/kotlinchatapp/LoginFragment.kt
@@ -1,15 +1,18 @@
 package cf.khanhsb.kotlinchatapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import cf.khanhsb.kotlinchatapp.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,8 +21,14 @@ class LoginFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.navToSignUpButton.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_loginFragment_to_signUpFragment2)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/cf/khanhsb/kotlinchatapp/SignUpFragment.kt
+++ app/src/main/java/cf/khanhsb/kotlinchatapp/SignUpFragment.kt
@@ -1,13 +1,19 @@
 package cf.khanhsb.kotlinchatapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import cf.khanhsb.kotlinchatapp.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,8 +22,16 @@ class SignUpFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
+        val view =
+        binding.navToSignInButton.setOnClickListener {
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
+
+        return binding.root
     }
 }
