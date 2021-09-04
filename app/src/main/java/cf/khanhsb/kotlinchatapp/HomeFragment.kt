2f7e2c5686f8bd744c957dbd17915b669ba106ae
KSB-tqk <linh05602@gmail.com>
2021-09-04 09:57:26 +0700

    fix code into MVVM

--- app/src/main/java/cf/khanhsb/kotlinchatapp/HomeFragment.kt
+++ app/src/main/java/cf/khanhsb/kotlinchatapp/HomeFragment.kt
@@ -4,21 +4,50 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import cf.khanhsb.kotlinchatapp.databinding.FragmentHomeBinding
+import cf.khanhsb.kotlinchatapp.viewmodel.HomeViewModel
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(){
+    override fun getViewBinding()= FragmentHomeBinding.inflate(layoutInflater)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun getViewModelClass() = HomeViewModel::class.java
 
+    override fun setUpViews() {
+        binding.logOutButton.setOnClickListener {
+            signOut()
+        }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private fun signOut() {
+        viewModel.signOut()
+        observeSignOut()
+    }
+
+    private fun observeSignOut() {
+        viewModel.signOutStatus.observe(viewLifecycleOwner, Observer { result ->
+            result?.let {
+                if (it.toString().equals("Success(Sign Out Successfully)")) {
+                    Toast.makeText(this.requireActivity(), "Sign Out Successfully", Toast.LENGTH_SHORT)
+                        .show()
+                    navigateToLoginFragment()
+                } else {
+                    Toast.makeText(
+                        this.requireActivity(),
+                        "Sign Out failed with ${it}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    println(it.toString())
+                }
+            }
+        })
+    }
+
+    private fun navigateToLoginFragment() {
+        findNavController().navigate(R.id.action_homeFragment_to_loginFragment)
     }
 }
